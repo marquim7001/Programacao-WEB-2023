ed5029815e7d11dee9ae9158b8c44dfe1d0b5b06
MARCOS ANTONIO SOARES DE SOUZA <marcos.souza@a.ucb.br>
2023-08-24 10:55:43 -0300

    revisando funções

--- funcoes/funcoes1.js
+++ funcoes/funcoes1.js
@@ -1,11 +1,30 @@
-function somar(a,b){
+function somar(a,b){ // chamando funçao basica de soma 
     if(b== undefined){
-
         return "NAO É POSSIVRL SOMAR";
     }
     return a+b;
 
 }
 
-console.log(somar(3,4));
-console.log(somar(3));
+// funçao recursiva 
+function contarAten(n){
+    if(n> -10){ // criterio de parada 
+    
+        console.log(n);
+        contarAten(n-1);
+    }
+
+}
+
+contarAten(5);
+
+const calculadora = {
+    somar (a,b){
+        return a+b;
+    },
+    subtrair: function(a,b){
+        return a-b;
+    }
+
+}
+console.log(calculadora.subtrair (1,3));
